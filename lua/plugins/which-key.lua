e355d43f224ed593770106ad4f518ae4b54e3f94
Shantanu Shukla <shantanu.shukla10@gmail.com>
2026-08-29 12:51:22 +0530

    fix which-key floating window issue

---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,15 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- Customize which-key
+
+---@type LazySpec
+return {
+  {
+    "folke/which-key.nvim",
+    opts = {
+      win = {
+        no_overlap = false,
+      },
+    },
+  },
+}
